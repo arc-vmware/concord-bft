2e94fe87528be44fde46da75a8f88948567fdeb9
Pankaj <pkumarthapa@vmware.com>
2022-04-12 13:40:33 +0530

    Digest generation using OpenSSL library (SHA2_256 & SHA3_256 algos).

--- bftengine/src/bftengine/ReplicaImp.cpp
+++ bftengine/src/bftengine/ReplicaImp.cpp
@@ -1338,8 +1338,12 @@ void ReplicaImp::sendPartialProof(SeqNumInfo &seqNumInfo) {
       else
         commitSigner = CryptoManager::instance().thresholdSignerForOptimisticCommit(seqNum);
 
+<<<<<<< HEAD
       Digest tmpDigest;
       Digest digestHelper;
+=======
+      Digest tmpDigest, digestHelper;
+>>>>>>> Digest generation using OpenSSL library (SHA2_256 & SHA3_256 algos).
       digestHelper.calcCombination(ppDigest, getCurrentView(), seqNum, tmpDigest);
 
       const auto &span_context = pp->spanContext<std::remove_pointer<decltype(pp)>::type>();
@@ -4378,8 +4382,12 @@ ReplicaImp::ReplicaImp(const LoadedReplicaData &ld,
         else
           commitSigner = CryptoManager::instance().thresholdSignerForOptimisticCommit(seqNum);
 
+<<<<<<< HEAD
         Digest tmpDigest;
         Digest digestHelper;
+=======
+        Digest tmpDigest, digestHelper;
+>>>>>>> Digest generation using OpenSSL library (SHA2_256 & SHA3_256 algos).
         digestHelper.calcCombination(ppDigest, getCurrentView(), seqNum, tmpDigest);
 
         PartialCommitProofMsg *p = new PartialCommitProofMsg(
